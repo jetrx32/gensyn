fe19bf10bb1989d1b2d03ceae3176df9b18d8e43
jetrx32 <jetrx32@gmail.com>
2025-11-04 13:53:53 +0300

    Create gensyn_updatev064.sh

---
+++ gensyn_updatev064.sh
@@ -0,0 +1,30 @@
+
+
+
+# Клонирование репы gensyn rl-swarm и подготовка venv
+echo "Клонирование репы gensyn rl-swarm и подготовка venv"
+systemctl stop rl-swarm
+cd && \
+mkdir -p /root/rlsv064_bak && \
+cp /root/rl-swarm/swarm.pem /root/rlsv064_bak/swarm.pem && \
+cp /root/rl-swarm/userApiKey.json /root/rlsv064_bak/userApiKey.json && \
+cp /root/rl-swarm/userData.json /root/rlsv064_bak/userData.json && \
+rm -rf rl-swarm && \
+git clone https://github.com/gensyn-ai/rl-swarm && \
+cd rl-swarm && git checkout v0.6.4 && python3 -m venv .venv && \
+cp /root/rlsv064_bak/swarm.pem /root/rl-swarm/swarm.pem && \
+cp /root/rlsv064_bak/userApiKey.json /root/rl-swarm/userApiKey.json && \
+cp /root/rlsv064_bak/userData.json /root/rl-swarm/userData.json && \
+# Скачивание кастомного скрипта на запуск ноды
+echo " Скачивание кастомного скрипта на запуск ноды"
+curl -o /root/rl-swarm/run_rl_swarm_true.sh https://raw.githubusercontent.com/JeTr1x/aztec_inst/refs/heads/main/run-rlsw-new.sh
+
+
+# Скачивание сервисника
+curl -o /etc/systemd/system/rl-swarm.service https://raw.githubusercontent.com/JeTr1x/gensyn-install/refs/heads/main/rl-swarm.service
+
+# Запуск  сервисника и вывод логов
+systemctl daemon-reload && \
+systemctl enable rl-swarm.service && \
+systemctl start rl-swarm.service && \
+journalctl -fu rl-swarm
