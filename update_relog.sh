ff219af11cf9b5764213625918fd2ad9bc16e986
jetrx32 <jetrx32@gmail.com>
2025-11-05 17:34:08 +0300

    Create update_relog.sh

---
+++ update_relog.sh
@@ -0,0 +1,26 @@
+
+
+
+# Клонирование репы gensyn rl-swarm и подготовка venv
+echo "Клонирование репы gensyn rl-swarm и подготовка venv"
+systemctl stop rl-swarm
+cd && \
+mkdir -p /root/rlsv064_bak && \
+cp /root/rl-swarm/swarm.pem /root/rlsv064_bak/swarm.pem
+rm -rf rl-swarm && \
+git clone https://github.com/gensyn-ai/rl-swarm && \
+cd rl-swarm && git checkout v0.6.4 && python3 -m venv .venv && \
+cp /root/rlsv064_bak/swarm.pem /root/rl-swarm/swarm.pem && \
+# Скачивание кастомного скрипта на запуск ноды
+echo " Скачивание кастомного скрипта на запуск ноды"
+curl -o /root/rl-swarm/run_rl_swarm_true.sh https://raw.githubusercontent.com/jetrx32/gensyn/refs/heads/main/run_rl_swarm064.sh
+
+
+# Скачивание сервисника
+curl -o /etc/systemd/system/rl-swarm.service https://raw.githubusercontent.com/JeTr1x/gensyn-install/refs/heads/main/rl-swarm.service
+
+# Запуск  сервисника и вывод логов
+systemctl daemon-reload && \
+systemctl enable rl-swarm.service && \
+systemctl start rl-swarm.service && \
+journalctl -fu rl-swarm
